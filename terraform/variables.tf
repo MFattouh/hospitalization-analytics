0bb1842d2d7f1462142c6a69658a44c207fe35c1
Mohammad Fattouh <mo.fattouh@gmail.com>
2026-04-05 12:45:48 +0200

    feat: add terraform variables and provider config

---
+++ terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "gcp_project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "credentials_file" {
+  description = "Path to GCP service account JSON key file"
+  type        = string
+}
+
+variable "location" {
+  description = "GCP region for resources"
+  type        = string
+  default     = "US"
+}
+
+variable "bucket_name" {
+  description = "GCS datalake bucket name"
+  type        = string
+}
+
+variable "bq_dataset_raw" {
+  description = "BigQuery raw dataset name"
+  type        = string
+  default     = "raw"
+}
+
+variable "bq_dataset_staging" {
+  description = "BigQuery staging dataset name"
+  type        = string
+  default     = "staging"
+}
+
+variable "bq_dataset_marts" {
+  description = "BigQuery marts dataset name"
+  type        = string
+  default     = "marts"
+}
